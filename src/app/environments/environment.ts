e4ea8b431da1a68c86e1976dca5c03841dc26050
shiva <shivakumarjspro@gmail.com>
2024-06-05 20:25:14 +0530

    changed http to https.

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     production: false,
     //apiURL: 'http://167.71.231.191:8081',
     apiURL: 'http://167.71.231.191:8081',
-    adminBaseUrl: 'http://api.gradlit.in',
+    adminBaseUrl: 'https://api.gradlit.in',
     //adminBaseUrl: 'http://167.71.231.191:9874',
     authToken: '',
     adminToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJyb2xlX2lkIjoxLCJ1dWlkIjoiZTYwYzU3NDYtYjY0MS00ZTJhLThhNzUtYzg3ZmI0NTVhN2VjIiwiZXhwIjoxODIwNzQwNDUxfQ.4Z8ToI7NWXQnmFwCGDMdLIbV8Y9cIkmI88zv6UpxkLc'
